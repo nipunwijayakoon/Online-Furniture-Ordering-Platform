63beaa850b7faee5f32cb27667ae09ea4fa888ce
NIPUN <nipunchanakara@gmail.com>
2021-02-12 23:29:10 +0530

    addsearchbar

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -13,10 +13,15 @@ import Reset from './Component/Auth/resetpassword';
 import ViewDesign from './Component/Design/ViewDesign';
 import Navbar from './Component/Nav/Navbar';
 
-import Services from './Component/Services';
+import Shops from './Component/Services/Shops';
 import Products from './Component/Products';
-import Signin from './Component/Signin';
-import Signup from './Component/Signup';
+import Card1 from './Component/Table/Table1';
+import Card2 from './Component/Table/Table2';
+import Cardmini1 from './Component/Cardmini/Cardmini1';
+import Cardmini2 from './Component/Cardmini/Cardmini2';
+import Cardmini3 from './Component/Cardmini/Cardmini3';
+import Cardmini4 from './Component/Cardmini/Cardmini4';
+
 
 
 
@@ -48,10 +53,16 @@ const App = () => {
         <Route exact path='/ViewDesign' component={ViewDesign} />
         
         <Route path='/' exact component={Homepage} />
-          <Route path='/services' component={Services} />
+        <Route path='/services' component={Shops} />
           <Route path='/products' component={Products} />
-          <Route path='/sign-in' component={Signin} />
-          <Route path='/sign-up' component={Signup} />
+          <Route path='/sign-in' component={SignIn} />
+          <Route path='/sign-up' component={SignUp} />
+          <Route path='/card1' component={Card1} />
+          <Route path='/card2' component={Card2} />
+          <Route path='/cardmini1' component={Cardmini1} />
+          <Route path='/cardmini2' component={Cardmini2} />
+          <Route path='/cardmini3' component={Cardmini3} />
+          <Route path='/cardmini4' component={Cardmini4} />
       </Switch>
       
         </Fragment>
