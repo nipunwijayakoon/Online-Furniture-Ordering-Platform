aa3056e1816549a4d940ecb343739704aa2ce913
Charitha Maduranga <charithawd@gmail.com>
2021-02-15 12:20:11 +0530

    backendnewadded

---
+++ Frontend/my-app/ProjectBackend/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectBackEnd.Models
+{
+    public class Item
+    {
+        [Key]
+        public int ItemID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Content { get; set; }
+        public string Price { get; set; }
+        public string Colour { get; set; }
+        
+    }
+}
---
+++ Frontend/my-app/ProjectBackend/Models/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectBackEnd.Models
+{
+    public class Login
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Cannot leave this field empty")]
+        public string Password { get; set; }
+    }
+}
